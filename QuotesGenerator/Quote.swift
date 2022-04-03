5cefb11e460eb39a0859cb062223b0cacce6b252
pjs0418 <tony180322@gmail.com>
2022-04-03 12:39:24 +0900

    feat: Complete project

---
+++ QuotesGenerator/Quote.swift
@@ -0,0 +1,13 @@
+//
+//  Quote.swift
+//  QuotesGenerator
+//
+//  Created by 준수의 MacBook on 2022/04/03.
+//
+
+import Foundation
+
+struct Quote {
+    let contents: String
+    let name: String
+}
